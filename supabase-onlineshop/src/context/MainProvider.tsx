ee3b6dbefe008e822173129a17917f8d4dcfc385
AbuTokio <bunyaminxkara@gmail.com>
2025-10-14 10:26:56 +0200

    new user - header

--- supabase-onlineshop/src/context/MainProvider.tsx
+++ supabase-onlineshop/src/context/MainProvider.tsx
@@ -2,19 +2,31 @@ import React, { createContext, useEffect, useState } from "react"
 import type { Product } from "../interfaces/Product"
 import { getProducts_store } from "../functions/getProduct"
 import type { Cart } from "../interfaces/Cart"
+import type { User } from "../interfaces/User"
 
 export interface MainContextProps {
   products: Product[]
   cart: Cart[] | null | undefined | unknown
   setCart: React.Dispatch<React.SetStateAction<Cart[] | null | undefined | unknown>>
+
+  // % NEW
+  user: User | null
+  setUser: React.Dispatch<React.SetStateAction<User | null>>
+  isLoggedIn: boolean
+  setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>
+  loading: boolean
 }
 
-// eslint-disable-next-line react-refresh/only-export-components
 export const mainContext = createContext<MainContextProps | null>(null)
 
 export default function MainProvider({ children }: { children: React.ReactNode }) {
   const [products, setProducts] = useState<Product[]>([])
   const [cart, setCart] = useState<Cart[] | null | undefined | unknown>([])
+
+  // % NEW
+  const [user, setUser] = useState<User | null>(null)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     const getData_In_useEffect = async () => {
@@ -34,6 +46,11 @@ export default function MainProvider({ children }: { children: React.ReactNode }
     products,
     cart,
     setCart,
+    user,
+    setUser,
+    isLoggedIn,
+    setIsLoggedIn,
+    loading,
   }
   return <mainContext.Provider value={value}>{children}</mainContext.Provider>
 }
